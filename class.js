19651cc1599253c03a0fbb50e2fb234eca048ccd
Benjamin Ikeh <b.ikeh@yahoo.com>
2023-02-08 11:40:53 +0000

    pagination

--- class.js
+++ class.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 
-(async() =>{
+let scrape = async() =>{
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: false,
@@ -12,7 +12,39 @@ const puppeteer = require("puppeteer");
     waitUntil: "load"
   })
 
-  const is_disabled = await page.$('a.')
-  
+  var results = [];
+  var lastPageNumber = 400
 
+  for (let index = 0; index < lastPageNumber; index++){
+    //await page.waitForTimeout(5000);
+
+    results = results.concat(await extractEvaluateCall(page))
+    if (index != lastPageNumber - 1){
+      await page.$('a .s-pagination-item .s-pagination-next .s-pagination-button')
+    }``
+  }
+  // browser.close()
+  return results;
+}
+
+async function extractEvaluateCall(page){
+  return page.evaluate(()=>{
+    let data = []
+    let elements = document.querySelectorAll('.sg-col-inner')
+
+    for (let element of elements){
+      //console.log(element)
+      let title = element.childNodes[5];
+      let price = element.childNodes[7];
+
+      data.push({ title, price })
+    }
+  })
+}
+
+scrape().then((value)=>{
+  console.log(value);
+  console.log("Collection length: "+ value.length);
+  console.log(value[0]);
+  console.log(value[value.length - 1])
 })
