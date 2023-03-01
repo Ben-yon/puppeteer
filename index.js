a02a9d6648cf3c90beb1083248dfcb4113514b5e
Benjamin Ikeh <b.ikeh@yahoo.com>
2023-03-01 16:27:13 +0000

    pagination issues fix

--- index.js
+++ index.js
@@ -7,39 +7,45 @@ const puppeteer = require('puppeteer');
     userDataDir: './tmp'
   });
   const page = await browser.newPage()
-  await page.goto('https://www.amazon.com/s?i=computers-intl-ship&rh=n%3A16225007011&fs=true&qid=1677168693&ref=sr_pg_1');
-  const productHandles = await page.$$('.sg-col-inner');
+  await page.goto('https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A16225007011%2Cp_89%3AApple&dc&fs=true&ds=v1%3Aju2opDf4k6YwsPIuTMqjVIB1%2FQaU4Xw3V2cRthXJOXc&qid=1677683905&rnid=2528832011&ref=sr_nr_p_89_1');
+
 
   const i = 0
   const items = []
 
-  for (const product of productHandles) {
-    let disabledLink = false
-    while(!disabledLink){
+  let disabledLink = false
+  while (!disabledLink) {
+    const productHandles = await page.$$('.sg-col-inner');
+    for (const product of productHandles) {
       let title = ""
       let price = ""
       let image = ""
 
       try {
         title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, product);
-      }catch (error) {}
-      try{
+      } catch (error) {}
+      try {
         price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, product)
-      }catch (error) {}
-      try{
+      } catch (error) {}
+      try {
         image = await page.evaluate(img => img.querySelector(".s-image").getAttribute("src"), product);
       } catch (error) {}
 
-      if (title !== ""){
-        items.push({title, price, image})
+      if (title !== "") {
+        items.push({
+          title,
+          price,
+          image
+        })
       }
+      //await page.waitForSelector('div > div > span > a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator')
       const is_disabled = await page.$$('span.s-pagination-item .s-pagination-next .s-pagination-disabled') !== null
-      //console.log(is_disabled)
-      if (!is_disabled === disabledLink){
-        try{
-          await page.click('div:nth-child(27) > div > div > span > a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator')
-        }
-        catch(error){}
+      disabledLink = is_disabled
+      if (is_disabled) {
+        try {
+          await page.click('div > div > span > a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator')
+          await page.waitForNavigation()
+        } catch (error) {}
       }
     }
   }
@@ -49,5 +55,5 @@ const puppeteer = require('puppeteer');
   console.log(items)
   //await page.screenshot({ path: 'mail.jpg'})
 
-  // await browser.close()
+  //await browser.close()
 })();
